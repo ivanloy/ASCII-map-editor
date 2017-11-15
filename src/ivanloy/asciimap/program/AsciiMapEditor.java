1fd89b554e0819aa06aacc6891b641d8514c1041
ivanloy <32630838+ivanloy@users.noreply.github.com>
2017-11-15 21:15:03 +0100

    Rect corners collisions
    Some fixes needed

--- src/ivanloy/asciimap/program/AsciiMapEditor.java
+++ src/ivanloy/asciimap/program/AsciiMapEditor.java
@@ -4,14 +4,23 @@ public class AsciiMapEditor {
 
 	public static void main(String[] args) {
 		
-		final short ROWS = 20;
-		final short COLS = 10;
+		final short ROWS = 50;
+		final short COLS = 16;
 		
 		Grid grid = new Grid(ROWS, COLS);
 		
-		grid.rect((short)1, (short)2, (short)11, (short)6);
-		grid.rect((short)3, (short)1, (short)7, (short)9);
-		grid.rect((short)0, (short)4, (short)16, (short)3);
+		grid.rect((short)15, (short)5, (short)10, (short)6);
+		grid.rect((short)24, (short)5, (short)12, (short)6);
+		grid.rect((short)4, (short)5, (short)12, (short)6);
+		grid.rect((short)15, (short)10, (short)10, (short)6);
+		grid.rect((short)15, (short)0, (short)10, (short)6);
+		grid.rect((short)24, (short)10, (short)12, (short)6);
+		grid.rect((short)4, (short)0, (short)12, (short)6);
+		grid.rect((short)24, (short)0, (short)12, (short)6);
+		grid.rect((short)4, (short)10, (short)12, (short)6);
+		
+		grid.rect((short)15, (short)0, (short)10, (short)6); //CORREGIR ESTE CASO (UNO ENCIMA DE OTRO) si ╦, dejarlo
+		
 		
 		//ajustar al borde si dos rectangulos estan al lado(juntos) IMPORTANTE
 		//cvuioasdhfliasdfashasdhvhsdfhsdifhsdfsdhfuisdhuyfgsduyfhsdhfuii
