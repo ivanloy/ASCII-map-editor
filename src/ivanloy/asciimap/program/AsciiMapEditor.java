7421f337831cdd481688ce513adc0bcc964a6282
ivanloy <32630838+ivanloy@users.noreply.github.com>
2017-11-17 14:27:22 +0100

    Line crosses fixed
    Need to fit some stuff

--- src/ivanloy/asciimap/program/AsciiMapEditor.java
+++ src/ivanloy/asciimap/program/AsciiMapEditor.java
@@ -4,40 +4,31 @@ public class AsciiMapEditor {
 
 	public static void main(String[] args) {
 		
-		final short ROWS = 50;
-		final short COLS = 16;
+		final short ROWS = 70;
+		final short COLS = 40;
 		
-		Grid grid = new Grid(ROWS, COLS);
+		Grid grid = new Grid(ROWS, COLS); //CREAR GRID
 		
-
-		grid.rect(15, 5, 10, 6); 
-		grid.rect(24, 5, 12, 6);
-		grid.rect(4, 5, 12, 6);
-		grid.rect(15, 10, 10, 6);
-		grid.rect(15, 0, 10, 6);
-		grid.rect(24, 10, 12, 6);
-		grid.rect(4, 0, 12, 6);
-		grid.rect(24, 0, 12, 6);
-		grid.rect(4, 10, 12, 6);
+		System.out.println(  "+==============================+\n"
+						   + "|            DRAW              |\n"
+						   + "+==============================|\n"
+						   + "|                              |\n"
+						   + "|      1. Horizontal line      |\n"
+						   + "|      2. Vertical line        |\n"
+						   + "|      3. Box                  |\n"
+						   + "|      0. Exit                 |\n"
+						   + "|                              |\n"
+						   + "+==============================+\n" );
 		
-		grid.rect((short)15, (short)0, (short)10, (short)6); //CORREGIR ESTE CASO (UNO ENCIMA DE OTRO) si ╦, dejarlo
-		
-		
-
-		grid.rect((short)15, (short)5, (short)10, (short)6);
-		grid.rect((short)24, (short)5, (short)12, (short)6);
-		grid.rect((short)4, (short)5, (short)12, (short)6);
-		grid.rect((short)15, (short)10, (short)10, (short)6);
-		grid.rect((short)15, (short)0, (short)10, (short)6);
-		grid.rect((short)24, (short)10, (short)12, (short)6);
-		grid.rect((short)4, (short)0, (short)12, (short)6);
-		grid.rect((short)24, (short)0, (short)12, (short)6);
-		grid.rect((short)4, (short)10, (short)12, (short)6);
-		
-		grid.rect((short)15, (short)0, (short)10, (short)6); //CORREGIR ESTE CASO (UNO ENCIMA DE OTRO) si ╦, dejarlo
-		
-		grid.printGrid();
-		
+		System.out.println(  "==============================\n"
+						   + "|            DRAW            |\n"
+						   + "==============================\n"
+						   + "|     1. Horizontal line     |\n"
+						   + "|     2. Vertical line       |\n"
+						   + "|     3. Box                 |\n"
+						   + "|                            |\n"
+						   + "|     0. Exit                |\n"
+						   + "==============================\n" );
 	}
 
 }
--- src/ivanloy/asciimap/program/Grid.java
+++ src/ivanloy/asciimap/program/Grid.java
@@ -43,14 +43,40 @@ public class Grid {
 	}
 	
 	public void printGrid() {
+		int rowCount = 0;
+		
+		System.out.print("    ");
+		for(short i = 0; i < cols; i++) {
+			
+			System.out.print(i/10); //poner hacia abajo 
+			
+		}
+		
+		System.out.println();
+		
+		System.out.print("    ");
+		for(short i = 0; i < cols; i++) {
+			
+			System.out.print(i%10); //poner hacia abajo 
+			
+		}
+		
+		System.out.println();
 		
 		for(short i = 0; i < rows; i++) {
+			
+			if(rowCount < 10) System.out.print(rowCount +"   ");
+			else if(rowCount < 100) System.out.print(rowCount +"  ");
+			else if(rowCount < 1000) System.out.print(rowCount +" ");
+			
 			for(short j = 0; j < cols; j++) {
 			
 				printTileChar(j, i);
 				
 			}
+			
 			System.out.println();
+			rowCount++;
 		}
 		
 	}
@@ -61,32 +87,41 @@ public class Grid {
 		rect((short)x, (short)y, (short)width, (short)height);
 		
 	}
-
-	public void rect(short x, short y, short width, short height) { //COMENTAR MEJOR TODO ESTO
+	
+	/** Prints a rectangle into the Grid using UTF-8 double line box drawing characters
+	 * 
+	 * @param x x coordinate of the up left corner of the rectangle
+	 * @param y y coordinate of the up left corner of the rectangle
+	 * @param width width of the rectangle
+	 * @param height height of the rectangle
+	 * 
+	 */
+	
+	public void rect(short x, short y, short width, short height) { //COMENTAR MEJOR TODO ESTO Y ORDENAR
 		
-		if(tile[x][y] == 2 || tile[x][y] == 5) tile[x][y] = 8;
-		else if(tile[x][y] == 1 || tile[x][y] == 4) tile[x][y] = 10;  
+		if(tile[x][y] == 2 || tile[x][y] == 5 || tile[x][y] == 8) tile[x][y] = 8;
+		else if(tile[x][y] == 1 || tile[x][y] == 4 || tile[x][y] == 10) tile[x][y] = 10;  
 		else if(tile[x][y] == 6 || tile[x][y] == 9 || tile[x][y] == 11 || tile[x][y] == 7) tile[x][y] = 7; //╬
 		else tile[x][y] = 3;                                             //╔
 		
 		hLine((short)(x+1), y, (short)(width-2));                   //═
 		hLine((short)(x+1), (short)(y+height-1), (short)(width-2)); //═
 		
-		if(tile[x+width-1][y] == 2 || tile[x+width-1][y] == 6) tile[x+width-1][y] = 9;
-		else if(tile[x+width-1][y] == 1 || tile[x+width-1][y] == 3) tile[x+width-1][y] = 10;  //╦
+		if(tile[x+width-1][y] == 2 || tile[x+width-1][y] == 6 || tile[x+width-1][y] == 9) tile[x+width-1][y] = 9;
+		else if(tile[x+width-1][y] == 1 || tile[x+width-1][y] == 3 || tile[x+width-1][y] == 10) tile[x+width-1][y] = 10;  //╦
 		else if(tile[x+width-1][y] == 5 || tile[x+width-1][y] == 8 || tile[x+width-1][y] == 11 || tile[x+width-1][y] == 7) tile[x+width-1][y] = 7;
 		else tile[x+width-1][y] = 4;                                     //╗
 		
 		vLine(x, (short)(y+1), (short)(height-2));                  //║
 		vLine((short)(x+width-1), (short)(y+1), (short)(height-2)); //║
 		
-		if(tile[x][y+height-1] == 2 || tile[x][y+height-1] == 3) tile[x][y+height-1] = 8;        //╠
-		else if(tile[x][y+height-1] == 1 || tile[x][y+height-1] == 6) tile[x][y+height-1] = 11;  //╩
+		if(tile[x][y+height-1] == 2 || tile[x][y+height-1] == 3 || tile[x][y+height-1] == 8) tile[x][y+height-1] = 8;        //╠
+		else if(tile[x][y+height-1] == 1 || tile[x][y+height-1] == 6 || tile[x][y+height-1] == 11) tile[x][y+height-1] = 11;  //╩
 		else if(tile[x][y+height-1] == 4 || tile[x][y+height-1] == 9 || tile[x][y+height-1] == 10 || tile[x][y+height-1] == 7) tile[x][y+height-1] = 7;
 		else tile[x][y+height-1] = 5;                                    //╚
 		
-		if(tile[x+width-1][y+height-1] == 2 || tile[x+width-1][y+height-1] == 4) tile[x+width-1][y+height-1] = 9; //╣
-		else if(tile[x+width-1][y+height-1] == 1 || tile[x+width-1][y+height-1] == 5) tile[x+width-1][y+height-1] = 11;
+		if(tile[x+width-1][y+height-1] == 2 || tile[x+width-1][y+height-1] == 4 || tile[x+width-1][y+height-1] == 9) tile[x+width-1][y+height-1] = 9; //╣
+		else if(tile[x+width-1][y+height-1] == 1 || tile[x+width-1][y+height-1] == 5 || tile[x+width-1][y+height-1] == 11) tile[x+width-1][y+height-1] = 11;
 		else if(tile[x+width-1][y+height-1] == 3 || tile[x+width-1][y+height-1] == 8 || tile[x+width-1][y+height-1] == 10 || tile[x+width-1][y+height-1] == 7) tile[x+width-1][y+height-1] = 7;
 		else tile[x+width-1][y+height-1] = 6;                            //╝
 		
@@ -96,18 +131,22 @@ public class Grid {
 		
 		for(short i = x; i < x + length; i++) {
 			
-			if(tile[i][y] == 2) tile[i][y] = 7; //╬
+			if(tile[i][y] == 2 || tile[i][y] == 7 || tile[i][y] == 8 || tile[i][y] == 9) tile[i][y] = 7; //╬
+			else if(tile[i][y] == 3 || tile[i][y] == 4 || tile[i][y] == 10) tile[i][y] = 10; //╦
+			else if(tile[i][y] == 5 || tile[i][y] == 6 || tile[i][y] == 11) tile[i][y] = 11; //╩
 			else tile[i][y] = 1;                //═
 			
 		}
 		
 	}
 	
-	public void vLine(short x, short y, short length) {
-		
+	public void vLine(short x, short y, short length) { //COMENTAR TODO MEJOR Y HACER JAVADOC
+		//TO DO 1 HACER QUE SI EL PRIMER O ULTIMO CARACTER COINCIDEN CON LA LINEA PERPENDICULAR, PONGA LA T Y NO EL CRUCE, SI NO ES DE UN CUADRADO
 		for(short i = y; i < y + length; i++) {
 			
-			if(tile[x][i] == 1) tile[x][i] = 7; //╬
+			if(tile[x][i] == 1 || tile[x][i] == 7 || tile[x][i] == 10 || tile[x][i] == 11) tile[x][i] = 7; //╬
+			else if(tile[x][i] == 3 || tile[x][i] == 5 || tile[x][i] == 8) tile[x][i] = 8; //╠
+			else if(tile[x][i] == 4 || tile[x][i] == 6 || tile[x][i] == 9) tile[x][i] = 9; //╣
 			else tile[x][i] = 2;                //║
 			
 		}
